4c541af2206c681c58c5392ae19fa26829090398
Test <Hammertyme101@gmail.com>
2023-09-08 16:32:42 -0400

    lab done

--- js/game.js
+++ js/game.js
@@ -1,3 +1,32 @@
 class Game {
-    // code to be added
+    constructor(){
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen");
+        this.gameEndScreen = document.getElementById("game-end");
+        this.player = null
+        this.height = '600px'
+        this.width = '500px'
+        this.obstacles = []
+        this.score = 0
+        this.live = 3
+        this.gameIsOver = false
+        Game.player = new player(Gameame.gameScreen, 200, 500,100,150, "./images/car.png")
+    }
+    start(){
+        this.gameScreen.style.height = `${this.height}px`
+        this.gameScreen.style.width = `${this.width}px`
+        this.startScreen.style.display = "none"
+        this.gameScreen.style.display = "inheret"
+        this.gameLoop()
+    }
+    gameLoop(){
+        if(this.gameIsOver){
+            window.cancelAnimationFrame()
+        }
+        this.update()
+        window.requestAnimationFrame(()=>this.gameLoop())
+    }
+    update(){
+        this.player.move()
+    }
 }
--- js/script.js
+++ js/script.js
@@ -2,11 +2,30 @@ window.onload = function () {
   const startButton = document.getElementById("start-button");
   const restartButton = document.getElementById("restart-button");
 
+  let game = new Game()
   startButton.addEventListener("click", function () {
     startGame();
+    
   });
 
   function startGame() {
-    console.log("start game");
+      console.log("start game");
+      game = new Game()
+      game.start()
+      
+      document.addEventListener('keydown', (e)=>{
+      if(e.key === "ArrowUp"){
+        game.player.directionY -= 1
+      }
+      if(e.key === "ArrowDown"){
+        game.player.directionY += 1
+      }
+      if (e.key === "ArrowLeft"){
+        game.player.directionX -= -1
+      }
+      if(e.key === "ArrowRight"){
+        game.player.directionX = 1
+      }
+    })
   }
 };
